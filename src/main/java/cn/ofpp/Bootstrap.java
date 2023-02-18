e3c05be2cbed530d3930fe68ed6bbcd91372050b
lunbbb <125731855+lunbbb@users.noreply.github.com>
2023-02-18 21:35:53 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wx8bdd8f0bca35f242";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "a429b8894d69d9705404a326e2b21aab";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "jRgwAexlE554KXtUClus5xDg0NupWhJKto5P0NrMzcQ";
 
     /**
      * 初始化
